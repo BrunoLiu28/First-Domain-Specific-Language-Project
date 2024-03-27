01c1bae49a0310cb44da5d096bd8dfc90ddcc189
Bruno Liu fc56297 <fc56297@alunos.fc.ul.pt>
2024-03-27 00:10:02 +0000

    tests made, need to print and maybe change the names in the model

--- test/scribble/ScribbleTester.java
+++ test/scribble/ScribbleTester.java
@@ -7,19 +7,47 @@ public class ScribbleTester {
 		TextProcessing processor = new TextProcessing();
 		String text = "This is an example text to be processed";
 		String text2 = "foxes foxing quick  The quick brown quick foxes jumped quick over quick the quick lazy quick dogs";
-//		ScribbleDSL tokenizeText = processor.withData(text)
-//											.tokenize("\\s+")
-//											.build();
-//		for (Token token : tokenizeText.getTextprocessor().getToken()) {
-//			System.out.println(token.getTokenName());
-//		}
-//		
+		ScribbleDSL tokenizeTest = processor.withData(text)
+											.tokenize("\\s+")
+											.build();
+		for (Token token : tokenizeTest.getTextprocessor().getToken()) {
+			System.out.println(token.getTokenName());
+		}
+		
+		ScribbleDSL stopWordRemovalTest = processor.withData(text)
+													.tokenize("\\s+")
+													.filterStopWords()
+													.build();
+		for (Token token : stopWordRemovalTest.getTextprocessor().getToken()) {
+			System.out.println(token.getTokenName());
+		}
+		
+		ScribbleDSL stemmingTest = processor.withData(text)
+											.tokenize("\\s+")
+											.performsStemming()
+											.build();
+		
+		for (Token token : stemmingTest.getTextprocessor().getToken()) {
+			System.out.println(token.getTokenName());
+		}
+		
+		ScribbleDSL wordFrequencyTest = processor.withData(text2)
+											 	.tokenize("\\s+")
+											 	.analyseWordFrequency()
+												.build();
+		
+		for (StringToInt stringint : wordFrequencyTest.getTextprocessor().getStringtoint()) {
+			System.out.println("the word \"" + stringint.getKey() + "\" appears " + stringint.getValue() + " time(s) in the text");
+		}
+
+
 //		ScribbleDSL analyseText = processor.fromFile("testFile.txt")
 //										 	.tokenize("\\s+")
 //										 	.filterStopWords()
 //										 	.performsStemming()
 //										 	.analyseWordFrequency()
 //											.build();
+		
 		ScribbleDSL analyseText = processor.withData(text2)
 										 	.tokenize("\\s+")
 										 	.filterStopWords()
